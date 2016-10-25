7f0bb092375b983e380415e17ebbc8e18e3caa8e
Murilo Henrique <henrybezerra10@outlook.com>
2016-10-25 14:32:11 -0300

    primeiro commit

---
+++ bolao/src/main/java/br/bolao/BolaoApplication.java
@@ -0,0 +1,12 @@
+package br.bolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoApplication.class, args);
+	}
+}
