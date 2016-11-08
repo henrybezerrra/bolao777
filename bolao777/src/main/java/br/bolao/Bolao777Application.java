ece08e793e18de1ed60867eff8121c295064674b
Murilo Henrique <Murilo Henrique@DELL.mshome.net>
2016-11-08 11:31:06 -0300

    Spring Security

---
+++ bolao777/src/main/java/br/bolao/Bolao777Application.java
@@ -0,0 +1,12 @@
+package br.bolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bolao777Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bolao777Application.class, args);
+	}
+}
