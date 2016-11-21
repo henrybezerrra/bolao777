74524b572c40d8b896db8e00909d35f694bc2f8e
Murilo Henrique <Murilo Henrique@DELL>
2016-11-21 08:33:46 -0300

    ...

--- bolao/src/main/java/br/bolao/controller/BolaoController.java
+++ bolao/src/main/java/br/bolao/controller/BolaoController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BolaoController {
 	
-	@RequestMapping("/bolao/novo")
-	public String novo(){
-		return "index.html";
+	@RequestMapping(value="/index")
+	public String fazeraposta(){
+		return "/fazeraposta";
 	}
 
 }
